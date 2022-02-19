62ad98e1637fed1db0aff824085b31d74d3af60b
Daniel Gomez <danielgomezwk@gmail.com>
2022-02-19 15:24:47 -0500

    Pushing intial server files to the repository

---
+++ phase4/capstoneProject/server.js
@@ -0,0 +1,24 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'dsaeotks',
+    host: 'jelani.db.elephantsql.com',
+    database: 'dsaeotks',
+    password: '7jCyv7wSMTQOUTHTEsRNsvjOCOLSJ6h1',
+    port: 5432,
+});
+
+client.connect();
+const query = `
+INSERT INTO garb(test)
+VALUES ('garbagedoe@gmail.com')
+`;
+
+client.query(query, (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Table is successfully created');
+    client.end();
+});
