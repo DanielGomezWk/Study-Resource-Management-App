813af2586e583de853660d952d528e76023cae95
Daniel Gomez <danielgomezwk@gmail.com>
2022-02-19 18:02:49 -0500

    Changes by Bran Changes by Craig
    Added login page

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//jshint esversion:6
 const { Client } = require('pg');
 
 const client = new Client({
@@ -8,18 +9,49 @@ const client = new Client({
     port: 5432,
 });
 
-client.connect();
-const query = `
-CREATE TABLE garBAG(
-    name varchar
-)
-`;
+// client.connect();
+// const query = `
+// CREATE TABLE garBAG(
+//     name varchar
+// )
+// `;
 
-client.query(query, (err, res) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log('Table is successfully created');
-    client.end();
+// client.query(query, (err, res) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log('Table is successfully created');
+//     client.end();
+// });
+
+
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public/"));
+const ejs = require("ejs");
+
+// user login/register Page
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+// TEMP
+app.post("/", (req, res) => {
+    res.redirect("/group");
+});
+
+app.get("/group", (req, res) => {
+    res.render("createGroup");
+});
+
+
+// launching app
+app.listen(3000, () => {
+    console.log("server is running");
 });
