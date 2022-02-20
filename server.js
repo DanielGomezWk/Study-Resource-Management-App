a24f0f313001291db90f8d77a6050cd2c907e588
Brandon Leger <51030112+Bran-Leger@users.noreply.github.com>
2022-02-19 20:02:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,11 @@ app.post("/", (req, res) => {
 // });
 
 
-// launching app
-app.listen(3000, () => {
-  console.log("server is running");
+// added server port
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server has started on port 3000");
 });
