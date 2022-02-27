1f1adfc08d9f444379b5caca425866df621edfd3
DanielGomezWk <85709362+DanielGomezWk@users.noreply.github.com>
2022-02-26 20:08:51 -0500

    Update server.js
    Added edge case for Deletion of group immediately after creation

--- server.js
+++ server.js
@@ -151,25 +151,27 @@ function makeGroup(req, res) {
   let gName = req.body.groupName;
   let priv = false;
 
+  //Creating new group
   const query = "INSERT INTO group_(group_id, leader, group_name, group_desc, private) VALUES($1, $2, $3, $4, $5)";
   const values = [gId, leaderEmail, gName, gDesc, priv];
 
   client.query(query, values, (err, response) => {
+
+    //Group unsuccessfully created
     if (err) {
       console.log("makeGroup broke!");
       console.log("------------------------------------");
       console.log(err.stack)
+      res.redirect("/group")
     } else {
       let joinDate = new Date;
       let inviteDate = joinDate;
-      let status = true
+      let status = true;
       const query = "INSERT INTO member_(email, groupid, status, joindate, invitedate) VALUES($1, $2, $3, $4, $5)";
       const values = [leaderEmail, gId, status, joinDate, inviteDate];
 
       client.query(query, values, (err, response) => {
         if (err) {
-          console.log("makeGroup broke! again");
-          console.log(err.stack);
           console.log("makeGroup broke! again");
           console.log(err.stack);
           const query = 'DELETE FROM "group_" WHERE ' +
